7b9dfbbc250e16a9ee66a2f05f3c6c0c70ae8ae9
sxh1136 <49796522+sxh1136@users.noreply.github.com>
2025-03-03 13:55:03 +0800

    Update p-distance-calc.py

--- webapp/p-distance-calc.py
+++ webapp/p-distance-calc.py
@@ -1,6 +1,6 @@
 import sys
 import json
-from Bio import SeqIO, Align
+from bio import SeqIO, Align
 
 def calculate_p_distance(seq1, seq2):
     # Create a PairwiseAligner object with appropriate scoring
